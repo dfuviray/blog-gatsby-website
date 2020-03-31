c966a3d4c60f042b6aa0f553e1820f310d0d1d7d
dfuviray <deo.uviray@gmail.com>
2020-03-31 22:43:03 +0800

    add row blog page

--- src/components/blog-section/Blog.js
+++ src/components/blog-section/Blog.js
@@ -33,25 +33,29 @@ const Blog = () => {
 
   return (
     <BlogWrapper>
-      <div className="col col-md-8 mx-auto">
-        <Pagination totalCount={totalCount} currentPage={1} />
-      </div>
-      {data.map((post, index) => (
-        <div key={index} className="col col-md-8 mx-auto post">
-          <Link
-            to={`post/${post.node.title
-              .toLowerCase()
-              .split(" ")
-              .join("-")}`}
-          >
-            <h3 className="title">{post.node.title}</h3>
-          </Link>
-          <p className="author">{post.node.author}</p>
-          <Content
-            content={post.node.content.json.content[0].content[0].value || null}
-          />
+      <div className="row">
+        <div className="col col-md-8 mx-auto">
+          <Pagination totalCount={totalCount} currentPage={1} />
         </div>
-      ))}
+        {data.map((post, index) => (
+          <div key={index} className="col col-md-8 mx-auto post">
+            <Link
+              to={`post/${post.node.title
+                .toLowerCase()
+                .split(" ")
+                .join("-")}`}
+            >
+              <h3 className="title">{post.node.title}</h3>
+            </Link>
+            <p className="author">{post.node.author}</p>
+            <Content
+              content={
+                post.node.content.json.content[0].content[0].value || null
+              }
+            />
+          </div>
+        ))}
+      </div>
     </BlogWrapper>
   )
 }
