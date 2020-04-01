d3b4a264f0de4b578c1252f2b65782ddac7d52d1
dfuviray <deo.uviray@gmail.com>
2020-04-01 14:37:16 +0800

    import bootstrap in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Blog from "../components/blog-section/Blog"
 
+import "bootstrap/dist/css/bootstrap.css"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
